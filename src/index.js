ae01fe2dc12fc66d04b70ff2d96e1854ddafc0a4
Miah Diaz <91426819+meohmiahh@users.noreply.github.com>
2022-03-23 22:07:59 -0500

    Add button to add new note

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import "./styles.css";
+const notesContainer = document.getElementById("app");
+const addNoteButton = notesContainer.querySelector(".add-note");
+
+function getNotes() {}
+
+function saveNotes(notes) {}
+
+function createNotesElement(id, content) {}
+
+function addNote() {}
+
+function updateNote(id, newContent) {}
+
+function deleteNote(id, element) {}
